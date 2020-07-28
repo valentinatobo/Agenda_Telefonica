e25cb9d7c86de5fb48e551a0a336dcc93cba7232
valentinatobo <valentina.uscategui99@gmail.com>
2020-07-28 09:58:48 -0500

    sin terminar

---
+++ javas/js/script.js
@@ -0,0 +1,62 @@
+let contactos = [];
+
+$(document).ready(function(){
+
+    cargarContactos();
+
+    $('#agregarContacto').on('click',function(){
+        
+        let formulario = $(this).parent().parent();
+        incluirContacto(formulario.find('#txtNombre').val(), formulario.find('#txtTelefono').val(), formulario.find('#txtEmail').val
+        ());
+
+    });
+
+    function cambiarColorTarjeta(){
+        $('.card').removeClass('text-white bg-info');
+        $(this).addClass('text-white bg-info');
+    }
+    
+    function cargarContactos(){
+       $.ajax({
+            type: "get",
+            url: 'contactos.json',
+            success: function(data){
+                for(let i=0; i<data.length; i++){
+                    incluirContacto(data[i].nombre, data[i].telefono, data[i].email);
+                }  
+            }
+        });
+    }
+    
+    function incluirContacto(nombre, telefono, email){
+
+        const contacto = new Contacto(nombre, telefono, email);
+        contactos.push(contacto);
+     
+
+        let row = $('<div class-"row"></div>');
+        row.prepend('<div class-"col-12"></div>')
+        row.find('.col-12').prepend('<div class="card mb-3"></div>');
+        row.find('.card').prepend('<div class="row no-gutters"></div>');
+        row.find('.row').prepend('<div class="col-md-4"></div>');
+        row.find('.col-md-4').prepend('<img src="img/user=perfil.png" class="card-img">');
+        row.find('.row').append('<div class="col-md-8"></>');
+        row.find('.col-md-8').prepend('<div class="card-body"></>');
+        row.find('.card-body').append('<h5 class="card-title">' + contacto.nombre +'</h5>');
+        row.find('.card-body').append('<p class="card-text">' + contacto.telefono + '</p>');
+        row.find('.card-body').append('<p class="card-text">' + contacto.email + '</p>');
+        $('#listaContactos').find('#titulo2');
+    }
+
+});
+
+
+class Contacto{
+
+    constructor(nombre, telefono, email){
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+}
